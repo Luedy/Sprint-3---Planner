f8d301b1eae18f8240a6b6087a2ddf21f6131f21
Lucas Luedy <luedyrego@gmail.com>
2022-10-04 08:27:43 -0300

    task

--- js/index.js
+++ js/index.js
@@ -94,3 +94,57 @@ function favTutorial() {
     mylist.options[mylist.selectedIndex].text;
   console.log(document.getElementById("favourite").value);
 }
+
+
+function createdTask() {
+    const divideHours = hours.split(":");
+    const formatedHours = divideHours[0] + "h" + divideHours[1] + "m";
+    console.log();
+  
+    const schedule = document.createElement("div");
+    schedule.classList.add("to-do-schedule");
+    schedule.classList.add("teste");
+    const appointment = document.createElement("div");
+    appointment.classList.add("make-appointments");
+    const task = document.createElement("div");
+    task.classList.add("task");
+    schedule.appendChild(appointment);
+    schedule.appendChild(task);
+  
+    switch (weekDay) {
+      case "Segunda-feira":
+        task.classList.add("taskMon");
+        break;
+      case "Terça-feira":
+        task.classList.add("taskTues");
+        break;
+      case "Quarta-feira":
+        task.classList.add("taskWednes");
+        break;
+      case "Quinta-feira":
+        task.classList.add("taskThurs");
+        break;
+      case "Sexta-feira":
+        task.classList.add("taskFri");
+        break;
+      case "Sabado":
+        task.classList.add("taskSatur");
+        break;
+      case "Domingo":
+        task.classList.add("taskSun");
+        break;
+    }
+  
+    //create a forEach to add to the bank "ou seja criar um forEach para adicionar no banco" check
+  
+    //Nesse forEach ele deve criar esse atributo já personalizado check
+  
+    //apagar no banco  check
+  
+    //deletar todos check
+  
+    //criar abasde filtragem personalizadas para listar so os dias e um para default
+  
+    //horarios repetidos deveremos colocar um do lado do outro e colocar uma linha demarcando
+  
+    //finals create localStorege e dellLocalStorege

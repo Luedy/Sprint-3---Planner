1aa787d30a8ecbbafd2fe93e2c8f335bdb7239c7
Lucas Luedy <luedyrego@gmail.com>
2022-10-04 08:36:19 -0300

    funcionalidade js

--- js/index.js
+++ js/index.js
@@ -148,3 +148,298 @@ function createdTask() {
     //horarios repetidos deveremos colocar um do lado do outro e colocar uma linha demarcando
   
     //finals create localStorege e dellLocalStorege
+
+
+    appointment.innerHTML = `<h6>${formatedHours}</h6>`;
+
+  task.innerHTML = `  <div class="content">
+    <p>${textContent}</p>
+  </div>
+ <button class="out teste"> <!-- Delete -->
+    Apagar
+ </button>`;
+
+  document.querySelector(".schedules").appendChild(schedule);
+}
+
+
+const dataBase = [];
+
+
+//capture data;
+var textContent = document.querySelector(".activity").value;
+var weekDay = document.querySelector("#favourite").value;
+var hours = document.getElementById("input-time").value;
+
+let buttonAdd = document.querySelector(".add");
+
+//create task to facilited!
+const createTask = (textContent, hours, weekDay, indice) => {
+  let week;
+  switch (weekDay) {
+    case "Segunda-feira":
+      week = "taskMon";
+      break;
+    case "Terça-feira":
+      week = "taskTues";
+      break;
+    case "Quarta-feira":
+      week = "taskWednes";
+      break;
+    case "Quinta-feira":
+      week = "taskThurs";
+      break;
+    case "Sexta-feira":
+      week = "taskFri";
+      break;
+    case "Sabado":
+      week = "taskSatur";
+      break;
+    case "Domingo":
+      week = "taskSun";
+      break;
+  }
+
+     //console.log(dataBase.filter(task => task.weekDay == "Segunda-feira"))
+
+    
+
+  // if (dataBase.some(task => task.hours == hours) && dataBase.length > 2 ) {
+  //   console.log(hours)
+  //   console.log("cheguei aqui");
+  //   const found = dataBase.findIndex((task) => hours == task.hours);
+  //   // let all = document.querySelectorAll('.to-do-schedule');
+
+  //   // const allDivs = [...all]
+
+  //   let allTwo = document.getElementsByClassName('to-do-schedule');
+  //   allTwo[found]
+  //   console.log(allTwo[found])
+  // }
+  
+  const task = document.createElement("div");
+  task.classList.add("to-do-schedule");
+  task.innerHTML = ` 
+    <div class="make-appointments teste ">
+        <p>${hours}</p>
+    </div>
+  
+    <div class="task ${week}"  >
+      <div class="content">
+        <p>${textContent}</p>
+      </div>
+    <button class="out" data-indice=${indice}> <!-- Delete -->
+        Apagar
+    </button>
+    </div>
+    `;
+    
+    document.querySelector(".schedules").appendChild(task);
+};
+
+const clearListTask = () => {
+    const todoSchedule = document.querySelector(".schedules");
+    while (todoSchedule.firstChild) {
+      todoSchedule.removeChild(todoSchedule.lastChild);
+    }
+  };
+
+  //list tasks
+const listTasks = () => {
+    clearListTask();
+    dataBase.forEach((task, indice) =>
+      createTask(task.textContent, task.hours, task.weekDay, indice)
+    );
+  };
+
+  //remove all task
+addEventListener("click", (e) => {
+    const elemento = e.target;
+  });
+  
+  //capture out button
+addEventListener("click", (e) => {
+    const elemento = e.target;
+  
+    if (elemento.classList.contains("out")) {
+      const ind = elemento.dataset.indice;
+      removeItem(ind);
+    }
+  
+    if (elemento.classList.contains("dell")) {
+      while (dataBase.length > 0) {
+        dataBase.forEach((item, indice) => removeItem(indice));
+      }
+      listTasks();
+    }
+  });
+
+  const removeItem = (ind) => {
+    dataBase.splice(ind, 1);
+    listTasks();
+  };
+
+  const addOneSide = () =>{
+  
+}
+
+
+
+//insert to database
+const insertTask = () => {
+    let textContent = document.querySelector(".activity").value;
+    let weekDay = document.querySelector("#favourite").value;
+    let hours = document.getElementById("input-time").value;
+    dataBase.push({
+      hours: `${hours}`,
+      textContent: `${textContent}`,
+      weekDay: `${weekDay}`,
+    });
+    listTasks();
+    // let schedule = document.querySelectorAll(".to-do-schedule");
+    // schedule.forEach((s) => console.log(s.getBoundingClientRect()));
+  };
+  
+  const listTasks2 = () => {
+    clearListTask();
+   
+    fic.forEach((task, indice) =>
+      createTask(task.textContent, task.hours, task.weekDay, indice)
+    );
+    
+  };
+
+  const fic = []
+
+//event
+document.addEventListener("click",e =>{
+  e.preventDefault();
+  const elemento  = e.target;
+  
+  if(elemento.classList.contains('day0')){
+   
+     
+    if(fic.length>0){
+      while (fic.length > 0) {
+        fic.pop()
+      }
+    }
+    
+    dataBase.filter(task => task.weekDay == "Segunda-feira").map(task => fic.push(
+      {hours: `${task.hours}`,
+      textContent: `${task.textContent}`,
+      weekDay: `${task.weekDay}`,}
+    ));
+    listTasks2();
+    
+  }
+
+  if(elemento.classList.contains('day1')){
+   
+     
+    if(fic.length>0){
+      while (fic.length > 0) {
+        fic.pop()
+      }
+    }
+    
+    dataBase.filter(task => task.weekDay == "Terça-feira").map(task => fic.push(
+      {hours: `${task.hours}`,
+      textContent: `${task.textContent}`,
+      weekDay: `${task.weekDay}`,}
+    ));
+    listTasks2();
+    
+  }
+  if(elemento.classList.contains('day2')){
+   
+     
+    if(fic.length>0){
+      while (fic.length > 0) {
+        fic.pop()
+      }
+    }
+    
+    dataBase.filter(task => task.weekDay == "Quarta-feira").map(task => fic.push(
+      {hours: `${task.hours}`,
+      textContent: `${task.textContent}`,
+      weekDay: `${task.weekDay}`,}
+    ));
+    listTasks2();
+    
+  }
+  if(elemento.classList.contains('day3')){
+   
+     
+    if(fic.length>0){
+      while (fic.length > 0) {
+        fic.pop()
+      }
+    }
+    
+    dataBase.filter(task => task.weekDay == "Quinta-feira").map(task => fic.push(
+      {hours: `${task.hours}`,
+      textContent: `${task.textContent}`,
+      weekDay: `${task.weekDay}`,}
+    ));
+    listTasks2();
+    
+  }
+  if(elemento.classList.contains('day4')){
+   
+     
+    if(fic.length>0){
+      while (fic.length > 0) {
+        fic.pop()
+      }
+    }
+    
+    dataBase.filter(task => task.weekDay == "Sexta-feira").map(task => fic.push(
+      {hours: `${task.hours}`,
+      textContent: `${task.textContent}`,
+      weekDay: `${task.weekDay}`,}
+    ));
+    listTasks2();
+    
+  }
+  if(elemento.classList.contains('day5')){
+   
+     
+    if(fic.length>0){
+      while (fic.length > 0) {
+        fic.pop()
+      }
+    }
+    
+    dataBase.filter(task => task.weekDay == "Sabado").map(task => fic.push(
+      {hours: `${task.hours}`,
+      textContent: `${task.textContent}`,
+      weekDay: `${task.weekDay}`,}
+    ));
+    listTasks2();
+    
+  }
+  if(elemento.classList.contains('day6')){
+   
+     
+    if(fic.length>0){
+      while (fic.length > 0) {
+        fic.pop()
+      }
+    }
+    
+    dataBase.filter(task => task.weekDay == "Domingo").map(task => fic.push(
+      {hours: `${task.hours}`,
+      textContent: `${task.textContent}`,
+      weekDay: `${task.weekDay}`,}
+    ));
+    listTasks2();
+    
+  }
+ 
+  
+})
+
+
+listTasks();
+buttonAdd.onclick = insertTask;
